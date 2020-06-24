618c097d7acca1f5387ee1c48acdac643febd153
Jason Junio <jjjunio@gmail.com>
2020-06-24 23:28:21 +0000

    added product and category counts to admin products page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @product_count = Product.all.length
+    @category_count = Category.all.length
   end
 end
